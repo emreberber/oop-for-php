fd3a6932d524d288424ee9d75b8a49928e8194c8
Emre Berber <brbr.emre@gmail.com>
2018-01-26 14:41:47 +0300

    update 01.php

--- 01.php
+++ 01.php
@@ -1,33 +1,60 @@
 <?php
 
+// DRY -- DON'T REPEAT YOURSELF
+// self:: 
+// parent::
 
-class Cat 
+class Animal
 {
-    private $name  = "Duman";
-    private $color = "Gray";
-    private $sound = "Miyav";
+    protected $name;    // ACCESS FROM CHILD CLASSES
+    private $color;
+    private $sound;
+
+    const LIVE = true;
 
     public function getSound(){
-        echo $this->sound;  // NEW LINE FOR TERMINAL
-    }
-
-    public function setName(String $newName){
-        $this->name = $newName;
+        return $this->sound;    // NEW LINE FOR TERMINAL
     }
 
     public function getName(){
-        echo $this->name;
+        return $this->name;
     }
+
 }
 
-$obj = new Cat();  // CREATE A NEW OBJECT
-// echo $obj->name; CANNOT ACCESS PRIVATE PROPERTY
 
-$obj->getSound();
+class Cat extends Animal
+{
+    const age = 10;
+
+    public function __construct($name, $color, $sound){
+        $this->name  = $name;
+        $this->color = $color;
+        $this->sound = $sound; 
+    }
+
+    public function __destruct(){
+        //
+    }
+
+    public function getAge(){
+        return self::age;   // ACCESS TO THIS CLASS'S CONST
+    }
+
+    public function getLive(){
+        return parent::LIVE;   //  ACCESS TO PARENT CLASS'S CONST
+    }
+
+}
+
+
+$obj = new Cat('Sweetcat', 'White', 'Miyav');
+echo $obj->getLive();
 echo PHP_EOL;
-$obj->setName('Boncuk');
-$obj->getName();
-
+echo $obj->getAge();
 
 
 echo PHP_EOL; 
+
+
+// 02-12
